f4fe598c7ecb9302afc9646923f2f5af9d7a29e4
hayden <cfduan@outlook.com>
2023-07-06 12:32:19 +0800

    init

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"github.com/btcsuite/btcd/rpcclient"
-	"log"
 )
 
 type Transaction struct {
@@ -33,6 +32,45 @@ type Block struct {
 
 func main() {
 	// 替换为您的比特币节点的URL和区块哈希
+	//connCfg := &rpcclient.ConnConfig{
+	//	Host:         "localhost:8332",
+	//	User:         "coreincp",
+	//	Pass:         "oHGkzaGPRPcWX3xz",
+	//	HTTPPostMode: true,
+	//	DisableTLS:   true,
+	//}
+	//
+	//client, _ := rpcclient.New(connCfg, nil)
+	//
+	//blockHash, err := client.GetBlockHash(793980)
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//
+	//block, err := client.GetBlock(blockHash)
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//
+	//// 遍历每个交易并打印详细信息
+	//for _, tx := range block.Transactions {
+	//	txHash := tx.TxHash()
+	//	fmt.Printf("TxID: %s\n", txHash.String())
+	//
+	//	// 输出交易输入信息
+	//	for _, input := range tx.TxIn {
+	//		fmt.Println("Input:")
+	//		fmt.Printf("  TxID: %s\n", input.PreviousOutPoint.Hash.String())
+	//		fmt.Printf("  Output Index: %d\n", input.PreviousOutPoint.Index)
+	//	}
+	//}
+	if err := GetBlock1(793980); err != nil {
+		fmt.Errorf("err:", err.Error())
+		return
+	}
+}
+
+func GetBlock1(height int64) error {
 	connCfg := &rpcclient.ConnConfig{
 		Host:         "localhost:8332",
 		User:         "coreincp",
@@ -43,26 +81,61 @@ func main() {
 
 	client, _ := rpcclient.New(connCfg, nil)
 
-	blockHash, err := client.GetBlockHash(793980)
+	hash, err := client.GetBlockHash(height)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Errorf("GetBlock GetBlockHash err:", err.Error())
+		return err
 	}
 
-	block, err := client.GetBlock(blockHash)
+	rawBlock, err := client.GetBlock(hash)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Errorf("GetBlock GetBlockVerboseTx:", err.Error())
+		return err
 	}
 
-	// 遍历每个交易并打印详细信息
-	for _, tx := range block.Transactions {
-		txHash := tx.TxHash()
-		fmt.Printf("TxID: %s\n", txHash.String())
-
-		// 输出交易输入信息
-		for _, input := range tx.TxIn {
-			fmt.Println("Input:")
-			fmt.Printf("  TxID: %s\n", input.PreviousOutPoint.Hash.String())
-			fmt.Printf("  Output Index: %d\n", input.PreviousOutPoint.Index)
+	for key, rawTx := range rawBlock.Transactions {
+		if key > 3 {
+			return nil
 		}
+		fmt.Println("Index:", rawTx.TxHash().String())
+		for id, value := range rawTx.TxIn {
+			fmt.Println("input id:", id)
+			fmt.Println("Hash:", value.PreviousOutPoint.Hash)
+			fmt.Println("Index:", value.PreviousOutPoint.Index)
+
+			//if len(value.Witness) <= 1 {
+			//	continue
+			//}
+			//
+			//if len(value.Witness[1]) < 40 || !isInscribed(value.Witness[1]) {
+			//	continue
+			//}
+			//
+			//transaction := Transaction1{
+			//	Hash:        value.PreviousOutPoint.Hash.String(),
+			//	Index:       value.PreviousOutPoint.Index,
+			//	Txinwitness: value.Witness[1],
+			//}
+			//
+			//datatype, data, err := ExtractOrdFile(transaction.Txinwitness)
+			//
+			//if err != nil {
+			//	continue
+			//}
+			////println("block", sl.Height(), "has tx", tx.Hash.String(), "len", string(typ), "-", len(data), "bytes")
+			//if true {
+			//	ext := datatype
+			//	tps := strings.SplitN(string(datatype), "/", 2)
+			//	if len(tps) == 2 {
+			//		ext = tps[1]
+			//	}
+			//
+			//	fmt.Println("Hash: %s,type:%s,data:%s", transaction.Hash, transaction.Index, ext, string(data))
+			//}
+		}
+
+		fmt.Println("==============================")
 	}
+
+	return nil
 }
